164b3f1b60fc34546df22651ecf97215a6f97e55
fiha91 <rhfiha@gmail.com>
2022-11-10 12:50:39 +0600

    first-commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function colorBG(color) {
+  document.body.style.backgroundColor = color;
+
+  let text = document.getElementsByClassName("text");
+
+  if ((color == "#000000")) {
+    for (let elm of text) {
+      elm.style.color = "white";
+    }
+  } else {
+    for (let elm of text) {
+      elm.style.color = "#000000";
+    }
+  }
+}
